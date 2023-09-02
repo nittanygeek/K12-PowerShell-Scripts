8f3b4c4ce4cff332adc53fbb1ef6f9472d838cd9
Brian C. Hynds <hyndsb2077@clearfield.org>
2023-09-02 10:16:47 -0400

    Create Generate-ChatLogCSV.ps1

---
+++ Generate-ChatLogCSV.ps1
@@ -0,0 +1,52 @@
+# IMPORTANT: Open and mount the PST in Outlook before running the script
+# Generate a CSV file from a mounted PST file in Outlook containing chat messages
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$PSTPath,
+    [Parameter(Mandatory=$true)]
+    [string]$CSVPath
+)
+
+# Load the Outlook COM object
+$Outlook = New-Object -ComObject Outlook.Application
+
+# Load the PST file
+$PST = $Outlook.Session.Stores | Where-Object { $_.FilePath -eq $PSTPath }
+
+# Load the root folder of the PST file
+$RootFolder = $PST.GetRootFolder()
+
+# Load the folder containing the chat messages.  They are in a folder named "username@domain.com (Primary)" under a folder named "TeamsMessagesData"
+$ChatFolder = $RootFolder.Folders | Where-Object { $_.Name -eq "$($PST.DisplayName) (Primary)" } | ForEach-Object { $_.Folders | Where-Object { $_.Name -eq "TeamsMessagesData" } }
+
+# Load the chat messages
+$ChatMessages = $ChatFolder.Items | Where-Object { $_.MessageClass -eq "IPM.SkypeTeams.Message" }
+
+# Create a new CSV file
+$CSV = New-Object -TypeName System.IO.StreamWriter -ArgumentList $CSVPath
+
+# Write the header row
+$CSV.WriteLine("Date,From,To,Message")
+
+# Loop through the chat messages
+foreach ($ChatMessage in $ChatMessages) {
+
+    # Extract only the message between the body tags
+    $HTML = New-Object -Com "HTMLFile"
+    [string]$htmlBody = $ChatMessage.HTMLBody
+    $HTML.IHTMLDocument2_write($htmlBody)
+    $MessageBody = $HTML.body.innerText
+
+    if ($MessageBody -eq $null) {
+        $MessageBody = ""
+    }
+
+    # Write the message to the CSV file
+    $CSV.WriteLine("$($ChatMessage.ReceivedTime),$($ChatMessage.SenderName),$($ChatMessage.To),$($MessageBody)")
+}
+
+# Close the CSV file
+$CSV.Close()
+
+# Close Outlook
+$Outlook.Quit()
